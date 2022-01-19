aef6a44b6bb8439f7c0d8351438c260265db7302
jwhong <jwhong98@gmail.com>
2022-01-19 10:39:05 -0800

    basic layout

---
+++ src/components/Body/BodyElements.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem 0;
+  background-color: hsl(228, 33%, 97%);
+`;
